09d55f5840f1a07461ab82d903b199413ecb9f0e
Cselcuk89 <cagriselcuk89@gmail.com>
2023-10-16 13:22:35 -0500

    springboot thymeleaf integration part 2

--- Spring-09-Spring-Thymeleaf/src/main/java/com/example/spring09springthymeleaf/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/example/spring09springthymeleaf/controller/StudentController.java
@@ -3,14 +3,25 @@ package com.example.spring09springthymeleaf.controller;
 import com.example.spring09springthymeleaf.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
-    @RequestMapping("/register")
+
+    //    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    @GetMapping("/register")
     public String register(Model model){
+
         model.addAttribute("students", DataGenerator.createStudent());
+
         return "student/register";
+    }
+
+    @RequestMapping("/welcome")
+
+    public String info(){
+        return "templates/student/welcome";
     }
 }
